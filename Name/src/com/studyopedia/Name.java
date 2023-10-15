a749b6c4703ab3a04c2d363d48a21e27c8f8e1e5
User <User@LAPTOP-QUFJE5SL>
2023-10-16 00:31:48 +0800

    First Commit

---
+++ Name/src/com/studyopedia/Name.java
@@ -0,0 +1,11 @@
+package com.studyopedia;
+
+public class Name {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello my name is Fatin Amira");
+	}
+
+}
